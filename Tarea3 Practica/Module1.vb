2371ff2688af741377fd4a5d248f60004e954a34
Equipo <Equipo@DESKTOP-EAD5GPH>
2024-08-17 19:28:24 -0600

    Agregar archivos de proyecto.

---
+++ Tarea3 Practica/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim random As New Random()
+        Dim numeroSecreto As Integer = random.Next(1, 1001)
+
+        Dim adivinado As Boolean = False
+        Dim intento As Integer
+
+        Console.WriteLine("Adivina el número secreto entre 1 y 1000")
+
+        While Not adivinado
+            Console.Write("Ingrese el número que cree que se generó: ")
+            intento = Convert.ToInt32(Console.ReadLine())
+
+            If intento > numeroSecreto Then
+                Console.WriteLine("Ingrese el número que cree que se generó, pero más bajo.")
+            ElseIf intento < numeroSecreto Then
+                Console.WriteLine("Ingrese el número que cree que se generó, pero mayor.")
+            Else
+                Console.WriteLine("¡Felicidades, el número secreto era " & numeroSecreto & "!")
+                adivinado = True
+            End If
+        End While
+
+        Console.WriteLine("Presione cualquier tecla para salir...")
+        Console.ReadKey()
+    End Sub
+
+End Module
